8aaab8b244ecc381ae403c709a975f861b9ecafa
VVoev <vladovoev@yahoo.co.uk>
2017-12-12 22:01:33 +0200

    instantiate store and provider

--- reduxandreact/src/index.js
+++ reduxandreact/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navigation from './routes';
 
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 
+const store = configureStore();
 ReactDOM.render(
-    <Navigation />,
+    <Provider store={store}>
+        <Navigation />
+    </Provider>,
     document.getElementById('root')
 );
 
