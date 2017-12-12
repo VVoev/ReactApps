d6e2ee347adade0f40b79cd846fbff086892d47b
VVoev <vladovoev@yahoo.co.uk>
2017-12-12 23:13:46 +0200

    REDUX FLOW

--- reduxandreact/src/components/course/CoursePage.js
+++ reduxandreact/src/components/course/CoursePage.js
@@ -1,38 +1,62 @@
 import React, {PropTypes} from 'react'
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as courseActions from '../../actions/courseActions'
 
-class CoursePage extends React.Component{
-    constructor(props){
+class CoursePage extends React.Component {
+    constructor(props) {
         super(props)
-        this.state={
-            course:{title:""}
+        this.state = {
+            course: {title: ""}
         }
 
-        this.onTitleChange= this.onTitleChange.bind(this);
+        this.onTitleChange = this.onTitleChange.bind(this);
         this.onClickSave = this.onClickSave.bind(this)
     }
-    onTitleChange(event){
+
+    onTitleChange(event) {
         let course = this.state.course;
         course.title = event.target.value;
-        this.setState({course:course})
+        this.setState({course: course})
     }
-    onClickSave(){
-        alert(`Saving ${this.state.course.title}`)
-    }
-    render(){
 
+    onClickSave() {
+        this.props.actions.createCourse(this.state.course);
+    }
+
+    courseRow(course,index){
         return(
+            <li key={index}>{course.title}</li>
+        )
+    }
+
+    render() {
+
+        return (
             <div>
                 <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
                 <input type="text"
-                onChange={this.onTitleChange}
-                value={this.state.course.title}/>
+                       onChange={this.onTitleChange}
+                       value={this.state.course.title}/>
                 <input type="submit"
-                value="Save"
-                onClick={this.onClickSave}/>
+                       value="Save"
+                       onClick={this.onClickSave}/>
             </div>
         )
     }
 }
+function mapStateToProps(state, ownProps) {
+    return{
+        courses:state.courses
+    }
+}
 
-export default CoursePage;
+function mapDispatchToProps(dispatch) {
+    return{
+        actions: bindActionCreators(courseActions,dispatch)
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CoursePage);
